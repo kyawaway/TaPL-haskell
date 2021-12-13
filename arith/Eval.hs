c845708a9cccd3a56f6bb7c85654e2ac30c92fd3
tkyawa <kya@kyanoMacBook-Pro.local>
2021-12-13 15:49:52 +0900

    isnumericval

--- arith/Eval.hs
+++ arith/Eval.hs
@@ -4,26 +4,29 @@ import Control.Monad
 
 import Syntax
 
-
 eval1 :: Term -> Maybe Term
 eval1 t = case t of 
     (TmIf TmTrue t2 _) -> return t2
     (TmIf TmFalse _ t3) -> return t3
     (TmIf t1 t2 t3) -> liftM3 TmIf (eval1 t1) (return t2) (return t3)
     (TmSucc t) -> fmap TmSucc (eval1 t)
-    (TmPred (TmSucc t)) -> return t
+    (TmPred (TmSucc t)) -> if isnumericval t then return t else Nothing 
     (TmPred TmZero) -> return TmZero
     (TmPred t) -> fmap TmPred (eval1 t)
     (TmIsZero TmZero) ->  return TmTrue
-    (TmIsZero (TmSucc TmZero)) -> return TmFalse
+    (TmIsZero (TmSucc t)) -> if isnumericval t then return TmFalse else Nothing 
     (TmIsZero t) -> fmap TmIsZero (eval1 t)
     
     TmTrue -> Nothing 
     TmFalse -> Nothing 
     TmZero -> Nothing
 
-
+-- pred使うとnvの定義上直感的にアレな結果になりそう
 
 eval :: Term -> Term
 eval t =
   maybe t eval (eval1 t)
+
+
+-- 4.2.2.
+
